8989f4909bdef47cf901c09ab278d731ccd2d864
sine.traore@orangemali.com <sine.traore@orangemali.com>
2024-07-27 00:56:39 +0000

    first commit

---
+++ src/test/java/com/chat/chattingnotification/ChattingNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.chattingnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
